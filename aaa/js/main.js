7db968d39c8b241154e20bf14223338b8de68d74
hodahodahoda <xxxhodaxxx1502@gmail.com>
2024-10-10 05:59:17 +0900

    Add files via upload

--- aaa/js/main.js
+++ aaa/js/main.js
@@ -2,7 +2,7 @@ $(function(){
   // hamburger
   $('.hamburger').on('click', function() {
     $('.header').toggleClass('open');
-    $('body').toggleClass('no-scroll');
+    // $('body').toggleClass('no-scroll');
   });
   /*
   $('.hamburger').on('click', function() {
@@ -22,6 +22,40 @@ $(function(){
 
   $('#mask').on('click', function() {
     $('.header').removeClass('open');
-    $('body').removeClass('no-scroll');
+    // $('body').removeClass('no-scroll');
   })
+
+  // 電話リンクをクリックしたときはマスクを閉じないようにする
+  $('#mask a[href^="tel:"]').on('click', function(e) {
+    e.stopPropagation();  // クリックイベントの伝播を停止
+  });
+});
+
+
+// 初期オフセットの設定
+let offset = window.innerWidth <= 1170 ? 90 : 120;
+
+// ウィンドウのリサイズ時にオフセットを更新
+window.addEventListener('resize', () => {
+  if (window.innerWidth <= 1170) {
+    offset = 90; // 768px以下の場合のヘッダーの高さ
+    document.body.style.marginTop = "90px";
+  } else {
+    offset = 120; // 768px以上の場合のヘッダーの高さ
+    document.body.style.marginTop = "120px";
+  }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+
+    // スムーズスクロール
+    window.scrollTo({
+      top: target.offsetTop - offset,
+      behavior: 'smooth'
+    });
+  });
 });
